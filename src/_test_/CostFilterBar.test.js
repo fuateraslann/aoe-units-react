0f90900dea53102f521098495f9a15016b019c8d
fuateraslann <fuatze@gmail.com>
2022-03-08 20:51:08 +0300

    costFilter text test

--- src/_test_/CostFilterBar.test.js
+++ src/_test_/CostFilterBar.test.js
@@ -3,6 +3,7 @@ import { render,screen, fireEvent } from "@testing-library/react"
 import "@testing-library/jest-dom/extend-expect"
 import CostFilterBar from "../Components/UnitsPageComponents/CostsFilter/CostsFilterBar"
 
-it("renders correctly", () => {
-    render(<CostFilterBar setCostFilter={(arg)=>console.log(arg)} />)
+it("renders correctly and passed props true", () => {
+    render(<CostFilterBar setCostFilter={(arg)=>console.log(arg)} contentText = {"test"}/>)
+    expect(screen.getByText("test")).toBeInTheDocument()
 })
